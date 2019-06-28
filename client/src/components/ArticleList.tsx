dc88d629af8a9102456bd5e68f105128974fead1
Shan Huiyang <shanhuiyang@outlook.com>
2019-06-28 13:33:22 +0800

    reimplement article list

--- client/src/components/ArticleList.tsx
+++ client/src/components/ArticleList.tsx
@@ -5,6 +5,7 @@ import Article from "../models/Article";
 import { Link } from "react-router-dom";
 import User from "../models/User";
 import { byCreatedAt } from "../shared/date";
+import { Container, Segment, Label, Item, Button } from "semantic-ui-react";
 
 interface Props {
     state: AppState;
@@ -14,36 +15,53 @@ interface States {}
 
 class ArticleList extends React.Component<Props, States> {
     render(): React.ReactElement<any> {
-        return <div className="row">
+        return <Container text>
             {
                 this.props.state.articles.data
                 .sort(byCreatedAt).map(
                     (article: Article) => this._renderArticle(article)
                 )
             }
-        </div>;
+        </Container>;
     }
 
     private _renderArticle = (article: Article): React.ReactElement<any> => {
         const createDate: Date = article.createdAt ? new Date(article.createdAt) : new Date(0);
-        return <div className="col-sm-10 col-md-8 col-lg-7 bg-info" style={{margin: "8px"}} key={article._id}>
-            <h2>{article.title}</h2>
-            {this._renderAuthorInfo(article)}
-            <p style={{ whiteSpace: "pre-line" }}>{article.content}</p>
-            <p>
-                <span className="text-muted">{createDate.toLocaleString()}</span>
-                {this._renderEditButton(article)}
-            </p>
-        </div>;
+        const DESCRIPTION_PADDING = 10;
+        return <Segment>
+            <Item>
+                <Item.Content>
+                    <Item.Header as="h2">{article.title}</Item.Header>
+                    <Item.Meta>{this._renderAuthorInfo(article)}</Item.Meta>
+                    <Item.Description
+                        style={{
+                            whiteSpace: "pre-line",
+                            paddingTop: DESCRIPTION_PADDING,
+                            paddingBottom: DESCRIPTION_PADDING
+                        }}
+                        content={article.content}
+                    />
+                    <Item.Extra style={{
+                        display: "flex",
+                        flexDirection: "row",
+                        justifyContent: "space-between"}}>
+                        <div style={{color: "grey"}}>
+                            created at {createDate.toLocaleString()}
+                        </div>
+                        {this._renderEditButton(article)}
+                    </Item.Extra>
+                </Item.Content>
+            </Item>
+        </Segment>;
     }
 
     private _renderAuthorInfo = (article: Article): React.ReactElement<any> | undefined => {
         const authorInfo: User = this.props.state.articles.authors[article.author];
         if (authorInfo) {
-            return <p>
-                <img className="img-rounded profile" style={{width: "20px", height: "20px", marginRight: "6px"}} src={authorInfo.avatarUrl} alt={authorInfo.name}/>
-                <strong>{authorInfo.name}</strong>
-            </p>;
+            return <Label image color="teal">
+                <img src={authorInfo.avatarUrl} alt="avatar"/>
+                {authorInfo.name}
+            </Label>;
         } else {
             return undefined;
         }
@@ -52,9 +70,7 @@ class ArticleList extends React.Component<Props, States> {
     private _renderEditButton = (article: Article): React.ReactElement<any> | undefined => {
         if (article.author === (this.props.state.user && this.props.state.user._id)) {
             const uri: string = `/article/edit/${article._id}`;
-            return <Link className="btn btn-default pull-right" to={uri} style={{marginBottom: "8px"}} role="button">
-                    <i className="fa fa-edit"></i>Edit
-            </Link>;
+            return <Button primary as={Link} to={uri}><i className="fa fa-edit"></i>Edit</Button>;
         } else {
             return undefined;
         }
